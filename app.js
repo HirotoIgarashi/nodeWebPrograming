300c21ea4fcf05472a8b624c75b5ae258313a7d4
Hiroto Igarashi <hirotoigarashi@gmail.com>
2023-07-02 17:29:49 +0900

    2023-7-2 update

--- app.js
+++ app.js
@@ -2,26 +2,36 @@
 
 // expressのモジュールをロードする
 const express = require('express');
+// expressアプリケーションを定数appに代入する
+const app = express();
+
+// /apt/todos以下のパスに対するリクエストのハンドリングを
+// ./routes/todosモジュールに委譲する。
+// app.use('/api/todos', require('./routes/todos'));
 
 const todos = [
   { id: 1, title: 'ネーム', completed: false },
   { id: 2, title: '下書き', completed: true }
 ];
 
-// expressアプリケーションを定数appに代入する
-const app = express();
-
 // 待ち受けるポートを8000にする
-const port = 8000;
+const port = 3000;
 
 // /api/todosに対するGETリクエストを処理するハンドラ
-app.get('/api/todos', (req, res) =>
-  // ToDoの配列をJSON形式で返す
-  res.json(todos)
-)
-app.get("/", (req, res) => {
-  res.send("Hello, Universe!");
-})
+// ToDo一覧の取得
+// app.get('/api/todos', (req, res) => res.json(todos));
+app.get('/api/todos', (req, res) => {
+
+  console.log(req.query);
+
+  if (!req.query.completed) {
+    return res.json(todos);
+  }
+  // completedクエリパラメータを指定された場合はToDoをフィルタリング
+  const completed = req.query.completed === 'true';
+  res.json(todos.filter(todo => todo.completed === completed));
+});
+app.get('/', (req, res) => res.send("Hello, Universe!"))
 .listen(port, () => {
   console.log(`The Express.js server has started and is listening on port number:`
   + ` ${port}`);
