d70df32fdce6e264ee496ff65c483c983d2c8f30
Hiroto Igarashi <hirotoigarashi@gmail.com>
2023-07-21 00:02:02 +0900

    2023-7-21 update

--- trial/callback2.js
+++ trial/callback2.js
@@ -1,28 +1,27 @@
 'use strict';
 
-// 思い通りに動作しないパターン
+// callbackの使用例
 // consoleLogStart関数が非同期なためconsoleLogEndが先に実行されてしまう。
 // consoleLogEndをconsoleLogStart関数にcallbackとして渡して思い通りの
-// 順番に実行させたい。
+// 順番に実行させる。
 
 // 関数の定義
 const consoleLogStart = (text, callback) => {
   setTimeout(() => {
     console.log(`${text} start`);
-    callback;
-  }, 1000);
-};
-
-const consoleLogEnd = (text) => {
-  setTimeout(() => {
-    console.log(`${text} end`);
+    callback();
   }, 1000);
 };
 
 // 関数の実行
-consoleLogStart('process A', consoleLogEnd('process A'));
-// consoleLogStart('process A', () => {
-//   console.log('process A end');
-// });
-consoleLogStart('process B', consoleLogEnd('process B'));
-consoleLogStart('process C', consoleLogEnd('process C'));
+consoleLogStart('process A', () => {
+  console.log('process A end');
+});
+
+consoleLogStart('process B', () => {
+  console.log('process B end');
+});
+
+consoleLogStart('process C', () => {
+  console.log('process C end');
+});
