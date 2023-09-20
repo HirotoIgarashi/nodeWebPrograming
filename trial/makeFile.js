9205eef026703a738f9c78c4c43cd7588c850059
Hiroto Igarashi <hirotoigarashi@gmail.com>
2023-09-20 16:46:17 +0900

    2023-9-20 update

--- trial/makeFile.js
+++ trial/makeFile.js
@@ -4,24 +4,23 @@ import { stat, writeFile } from 'node:fs';
 
 const touchFile = ( fileName ) => {
   console.log(`${fileName}を作成します`); 
-  writeFile(fileName, '', 'utf8', ( err ) => {
+  writeFile( fileName, '', 'utf8', ( err ) => {
     if ( err ) {
       return console.log( err );
     }
-    console.log(`${fileName}を作成しました`);
+    console.log( `${fileName}を作成しました` );
   });
 };
 
 // statsは統計という意味
 const statsFile = ( fileName, callback ) => {
-  console.log(`引数は${fileName}です`);
-  stat(fileName, ( err, stats ) => {
+  stat( fileName, ( err, stats ) => {
     if ( err ) {
       if ( err.code === 'ENOENT' ) {
         console.log(`${fileName}というファイルはありません`);
         return callback();
       }
-      return console.log('不明なエラーが発生しました')
+      return console.log('不明なエラーが発生しました');
     } 
     console.log(
       `${fileName}がみつかりました。サイズは${stats.size}バイトあります。`
